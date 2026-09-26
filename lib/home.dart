c0a332d880f34a7270cd00e469101210bbd6b10d
Xena <atharizkysena@gmail.com>
2026-09-26 20:10:01 +0700

    Home Page Update => Menambahkan Tombol Trigger untuk Input suara dari pengguna dan menambahkan teks HALO PENGGUNA!

--- lib/home.dart
+++ lib/home.dart
@@ -12,16 +12,54 @@ class HomePage extends StatelessWidget {
           children: [
             Text(
               'Tekan Layar',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
+
             const SizedBox(height: 5),
 
             Text(
               'Tekan layar dimana pun',
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
 
-            const SizedBox(height: 5),
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(30),
+                side: BorderSide(
+                  color: Color(0xFFE1FF00), // Warna kuning neon
+                  width: 3, // Ketebalan garis border
+                ),
+              ),
+              child: const Icon(
+                Icons.graphic_eq, // Ganti dengan ikon pilihan Anda
+                color: Color(0xFFE1FF00),
+                size: 32,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text(
+              'Halo Pengguna!',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
           ],
         ),
       ),
